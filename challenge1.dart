7fe8e41f477f7ce47dc6b80cbfe60f6fe4a58009
Yousefq80 <ysalharby@gmail.com>
2022-11-08 17:45:12 +0300

    challenge 1 done

--- challenge1.dart
+++ challenge1.dart
@@ -1,8 +1,25 @@
+void main() {
+  String name = printName('yousef');
+  
+  
+  print(name);
+  printage(1979,2022);
+  printHello('es','yousef');
+  printMax(5, 20);
+
+}
+
 /**
  * Task 1:
  * Create a function called `printName`
  * - that prints your name on the screen.
  */
+String printName(String name) {
+  return 'Name:$name';
+}
+ 
+
+
 
 /**
  * Task 2:
@@ -12,6 +29,9 @@
  * - Age = current year - birth
  */
 
+void printage(int birthYear, int year){
+  print ("${ birthYear - year} years old")
+}
 /**
  * Task 3:
  * Create a function called `printHello`
@@ -22,10 +42,32 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
+void printHello(String lang, String name){
+if (lang == 'en') {
+    print("Hello $name");
+  }else if (lang == 'es') {
+    print("Hola $name");
+  }else if (lang == 'fr') {
+    print("Bonjour $name");
+  }
 
+
+}
 /**
  * Task 4:
  * Create a function called `printMax`
  * - that takes two numbers as parameters,
  * - and prints out the bigger number
  */
+
+void printMax(int x , int y){
+ 
+ if (x > y){
+  print (x);
+ } else {
+  print (y);
+ }
+
+
+
+}
